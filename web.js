f87afb7b9e96f12209e663eb37dd7cc68fd3909d
Vivien <vivien.gazeau.beta@gmail.com>
2013-07-10 08:42:07 -0400

    added index.html, web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+#!/usr/bin/env node
 var express = require('express');
+var fs = require('fs');
 
+var file = "BitStarter/index.html";
 var app = express.createServer(express.logger());
+var buf = new Buffer(256);
+
+buf = fs.readFileSync(file, "utf-8");
+console.log(buf);
+var sBuf = buf.toString("utf-8");
+console.log(sBuf);
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World2!');
+  response.send(sBuf);
 });
 
 var port = process.env.PORT || 5000;
